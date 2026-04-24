439c222265bce4b172553d7580e10328a2af6bf6
ddwq23143 <alanov.danila.12@gmail.com>
2026-04-24 09:24:11 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace Calculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Main main = new Main();
+            main.Run();
+        }
+    }
+}
